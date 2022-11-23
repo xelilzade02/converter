c9516cc9c20b4937f6a27a40059f25a277fd9805
unknown <hhsnov880@gmail.com>
2022-11-23 17:49:29 +0400

    addd

--- currency.js
+++ currency.js
@@ -1,169 +1,45 @@
-let base = "RUB";
-let symbols = "USD";
-let btnLeft = document.querySelectorAll(".left-currency button");
-let btnRight = document.querySelectorAll(".right-currency button");
-let inpLeft = document.querySelector(".inp-left");
-let inpRight = document.querySelector(".inp-right");
-let pLeft = document.querySelector(".p-left");
-let pRight = document.querySelector(".p-right");
-let url = "https://api.exchangerate.host/latest?";
-inpLeft.value = 1;
-FechRight(base, symbols)
-function Style() {
-  btnLeft.forEach((item) => {
-    item.addEventListener("click", function () {
-      btnLeft.forEach((item) => {
-        item.classList.remove("active");
-      });
-      base = this.innerHTML;
-      Api(this.parentElement.classList[0]);
-    });
-    item.addEventListener("click", () => {
-      item.classList.add("active");
-    });
-  });
-  btnRight.forEach((item) => {
-    item.addEventListener("click", function () {
-      btnRight.forEach((item) => {
-        item.classList.remove("active");
-      });
-      symbols = this.innerHTML;
-      Api(this.parentElement.classList[0]);
-    });
-    item.addEventListener("click", () => {
-      item.classList.add("active");
-    });
-  });
-}
-function onChangeLeft(evt){
-if(inpLeft.value[0]==0 &&inpLeft.value.length>1){
-  inpLeft.value=inpLeft.value[1]
-}
-}
-function onChangeRight(evt){
-if(inpRight.value[0]==0 &&inpRight.value.length>1){
-  inpRight.value=inpRight.value[1]
-  FechLeft(base, symbols);
-}
-}
-function inpEvent() {
-  inpLeft.addEventListener("input", () => {
-    if (inpLeft.value == "") {
-      inpRight.value = "";
-      pLeft.innerHTML = "";
-      pRight.innerHTML = "";
-    } else {
-      inpLeft.addEventListener('input',()=>{
-      });
-      FechRight(base, symbols);
-    }
-    if(inpLeft.value=="0"){
-      inpLeft.addEventListener('input', onChangeLeft, true);
-      
-    }
-  });
+const fromCurrency = document.querySelector(".fromCurrency")
+const toCurrency = document.querySelector(".toCurrency")
+const from = document.querySelector(".from");
+const to = document.querySelector(".to");
+const fromP = document.querySelector('.fromP')
+const toP = document.querySelector('.toP')
+const fromli = document.querySelectorAll('.fromCurrency li')
+const toli = document.querySelectorAll('.toCurrency li')
+let fr = "RUB";
+let tofr = "USD";
+let out;
+from.addEventListener('keyup',getData)
 
-  inpRight.addEventListener("input", () => {
-    if (inpRight.value == "") {
-      inpLeft.value = "";
-      pLeft.innerHTML = "";
-      pRight.innerHTML = "";
-    } else {
-      FechLeft(base, symbols);
-    }
-    if(inpRight.value=="0"){
-      inpRight.addEventListener('input', onChangeRight, true);
-    }
-  });
-}
-function Api(btn_parent_name) {
-  if (btn_parent_name == "right-currency") {
-    FechRight(base, symbols);
+fromCurrency.addEventListener(('click'), (e) => {
+  console.log(e.target.className);
+  if (e.target.className == 'from-li') {
+    fr = e.target.innerText
+    
   }
-  if (btn_parent_name == "left-currency") {
-    FechLeft(base, symbols);
+})
+toCurrency.addEventListener(('click'), (e) => {
+  console.log(e.target.className);
+  if (e.target.className == 'to-li') {
+    tofr = e.target.innerText
+    
+    
   }
+})
+function checkSelect() {
+  fromli.forEach((item) => {
+    item.classList.remove('selected')
+    if (fr == item.innerText) {
+      item.classList.add('selected')
+    }
+  })
+  toli.forEach((item) => {
+    item.classList.remove('selected')
+    if (tofr == item.innerText) {
+      item.classList.add('selected')
+    }
+  })
 }
 
-function FechLeft(baseFunc, symbolsFunc) {
-  if (baseFunc != symbolsFunc) {
-    fetch(`${url}base=${symbolsFunc}&symbols=${baseFunc}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        inpLeft.value =
-          Number(inpRight.value.replace(/\s+/g, "")) * data.rates[`${baseFunc}`];
-        pRight.innerHTML = `1${data.base}=${
-          data.rates[`${baseFunc}`]
-        }${baseFunc}`;
-        fetch(`${url}base=${baseFunc}&symbols=${symbolsFunc}`)
-          .then((res) => {
-            return res.json();
-          })
-          .then((data) => {
-            pLeft.innerHTML = `1${data.base}=${
-              data.rates[`${symbolsFunc}`]
-            }${symbolsFunc}`;
-          });
-      });
-  } else {
-    inpLeft.value = inpRight.value;
-    pLeft.innerHTML = `1${base}=1${base}`;
-    pRight.innerHTML = `1${base}=1${base}`;
-  }
-}
-function FechRight(baseFunc, symbolsFunc) {
-  if (baseFunc != symbolsFunc) {
-    fetch(`${url}base=${baseFunc}&symbols=${symbolsFunc}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        inpRight.value =
-          inpLeft.value.replace(/\s+/g, "") * data.rates[`${symbolsFunc}`];
-        pLeft.innerHTML = `1${data.base}=${
-          data.rates[`${symbolsFunc}`]
-        }${symbolsFunc}`;
-        fetch(`${url}base=${symbolsFunc}&symbols=${baseFunc}`)
-          .then((res) => {
-            return res.json();
-          })
-          .then((data) => {
-            pRight.innerHTML = `1${data.base}=${
-              data.rates[`${baseFunc}`]
-            }${baseFunc}`;
-          });
-      });
-  } else {
-    inpRight.value = inpLeft.value;
-    pLeft.innerHTML = `1${base}=1${base}`;
-    pRight.innerHTML = `1${base}=1${base}`;
-  }
-}
 
-  var numberMaskFrom = IMask(inpRight, {
-    mask: Number,
-    scale: 6,
-    signed: false,
-    thousandsSeparator: " ",
-    padFractionalZeros: false,
-    normalizeZeros: true,
-    radix: ".",
-    mapToRadix: [","],
-  });
-
-
-  var numberMaskTo = IMask(inpLeft, {
-    mask: Number,
-    scale: 6,
-    signed: false,
-    thousandsSeparator: " ",
-    padFractionalZeros: false,
-    normalizeZeros: true,
-    radix: ".",
-    mapToRadix: [","],
-  });
-
-Style();
-inpEvent();
+ 
