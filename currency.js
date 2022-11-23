7d6cb754bb8126c8b51048e20caf42892cf271bb
xelilzade02 <xelilzadeismayil48@gmail.com>
2022-11-23 21:22:37 +0400

    asdsadsdsd

--- currency.js
+++ currency.js
@@ -1,3 +1,5 @@
+// const { default: imask } = require("./imask");
+
 const fromCurrency = document.querySelector(".fromCurrency")
 const toCurrency = document.querySelector(".toCurrency")
 const from = document.querySelector(".from");
@@ -9,21 +11,55 @@ const toli = document.querySelectorAll('.toCurrency li')
 let fr = "RUB";
 let tofr = "USD";
 let out;
+checkSelect()
 from.addEventListener('keyup',getData)
-
+to.addEventListener('keyup',getData1)
+function getData() {
+  fetch(`https://api.exchangerate.host/latest?base=${fr}&symbols=${tofr}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      out = Object.values(data.rates)[0];
+      fromP.innerText = `1 ${fr} = ${out} ${tofr}`
+      toP.innerText = `1 ${tofr} = ${1 / out} ${fr}`
+      calcFrom(out)
+    }
+    )
+}
+function getData1() {
+  fetch(`https://api.exchangerate.host/latest?base=${tofr}&symbols=${fr}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      out = Object.values(data.rates)[0];
+      toP.innerText = `1 ${tofr} = ${out} ${fr}`
+      fromP.innerText = `1 ${fr} = ${1 / out} ${tofr}`
+      calcTo(out)
+    }
+    )
+}
+function calcFrom(out) {
+  to.value = out * from.value
+}
+function calcTo(out) {
+  from.value = out * to.value
+}
 fromCurrency.addEventListener(('click'), (e) => {
   console.log(e.target.className);
   if (e.target.className == 'from-li') {
     fr = e.target.innerText
-    
+    checkSelect()
+    getData()
   }
 })
 toCurrency.addEventListener(('click'), (e) => {
   console.log(e.target.className);
   if (e.target.className == 'to-li') {
     tofr = e.target.innerText
-    
-    
+    checkSelect()
+    getData()
   }
 })
 function checkSelect() {
@@ -41,5 +77,40 @@ function checkSelect() {
   })
 }
 
+const phoneMaskSelector = '.input';
+const phoneMaskInputs = document.querySelectorAll(phoneMaskSelector);
 
- 
+const masksOptions = {
+  phone: {
+    mask: '000 000 000 000 000 000 000 000 000 000 000 000 000 000'
+  }
+};
+
+for(const item of phoneMaskInputs) {
+  new IMask(item, masksOptions.phone);
+}
+
+// var numberMaskFrom = IMask(from, {
+//   mask: Number,
+//   scale: 6,
+//   signed: false,
+//   thousandsSeparator: " ",
+//   padFractionalZeros: false,
+//   normalizeZeros: true,
+//   radix: ".",
+//   mapToRadix: [","],
+// });
+
+
+// var numberMaskTo = IMask(to, {
+//   mask: Number,
+//   scale: 6,
+//   signed: false,
+//   thousandsSeparator: " ",
+//   padFractionalZeros: false,
+//   normalizeZeros: true,
+//   radix: ".",
+//   mapToRadix: [","],
+// });
+
+
